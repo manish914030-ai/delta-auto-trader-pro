8a25f75e59fbd0f4448969ce280fd6d8a2fff926
manish914030-ai <manish914030@gmail.com>
2026-08-06 06:26:21 +0530

    Add telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,1 +1,17 @@
+import requests
+from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
 
+
+def send_message(message):
+    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+
+    data = {
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": message,
+        "parse_mode": "HTML"
+    }
+
+    try:
+        requests.post(url, data=data, timeout=10)
+    except Exception as e:
+        print(f"Telegram Error: {e}")
