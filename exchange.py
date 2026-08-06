501734fffaeb326ec9e96d20e179340f5daff9ef
manish914030-ai <manish914030@gmail.com>
2026-08-06 06:24:24 +0530

    Add exchange.py

--- exchange.py
+++ exchange.py
@@ -1,1 +1,14 @@
+import ccxt
+from config import API_KEY, API_SECRET, TESTNET
 
+def get_exchange():
+    exchange = ccxt.delta({
+        "apiKey": API_KEY,
+        "secret": API_SECRET,
+        "enableRateLimit": True,
+    })
+
+    if TESTNET:
+        exchange.set_sandbox_mode(True)
+
+    return exchange
