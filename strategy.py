b4ce0801d7346dc251a07409177b379fbc125456
manish914030-ai <manish914030@gmail.com>
2026-08-06 09:21:25 +0530

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -1,12 +1,14 @@
-from config import EMA_FAST, EMA_SLOW, RSI_BUY, RSI_SELL
+from config import (
+    EMA_FAST,
+    EMA_SLOW,
+    RSI_BUY,
+    RSI_SELL
+)
+
 from indicators import add_indicators
 
 
 def check_signal(df):
-    """
-    Returns:
-        BUY, SELL or WAIT
-    """
 
     df = add_indicators(df)
 
@@ -16,20 +18,22 @@ def check_signal(df):
     last = df.iloc[-1]
     prev = df.iloc[-2]
 
-    # BUY Conditions
+    volume_ok = last["volume"] > df["volume"].rolling(20).mean().iloc[-1]
+
     buy = (
-        prev["EMA20"] <= prev["EMA50"] and
-        last["EMA20"] > last["EMA50"] and
-        last["RSI"] > RSI_BUY and
-        last["close"] > last["EMA20"]
+        prev["EMA20"] <= prev["EMA50"]
+        and last["EMA20"] > last["EMA50"]
+        and last["RSI"] > RSI_BUY
+        and last["close"] > last["EMA20"]
+        and volume_ok
     )
 
-    # SELL Conditions
     sell = (
-        prev["EMA20"] >= prev["EMA50"] and
-        last["EMA20"] < last["EMA50"] and
-        last["RSI"] < RSI_SELL and
-        last["close"] < last["EMA20"]
+        prev["EMA20"] >= prev["EMA50"]
+        and last["EMA20"] < last["EMA50"]
+        and last["RSI"] < RSI_SELL
+        and last["close"] < last["EMA20"]
+        and volume_ok
     )
 
     if buy:
